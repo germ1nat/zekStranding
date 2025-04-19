487f4beb0b8bdce230ffc4a2de34ee68dbdc442b
Eqqs <m.e.teymurlu@gmail.com>
2025-04-19 03:33:39 +0300

    feat: fixed PlayerMovement.cs to allow dashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,15 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayermOVEMENT : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
     public Rigidbody2D rb;
     public float WalkingSpeed = 850;
     public float RunningSpeed = 1125;
     private float Speed;
 
     private float activeSpeed;
-    public float dashSpeed;
+    public float dashSpeed = 2000; // Default value for dash speed
 
     public float dashLength = 0.5f, dashCooldown = 1f;
 
@@ -23,57 +22,65 @@ public class PlayermOVEMENT : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Speed = WalkingSpeed; // Initializessssss ssssssssspeed
+        activeSpeed = Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Dash();
-        }
-        else
-        {
-            Running();
-        }
-            
+        // Gets movement input
+        GetMovementInput();
+        
+        // Handles running / walking
+        HandleRunning();
+        
+        // Handles dashing
+        HandleDashing();
+        
+        // Applies movement
+        MovePlayer();
     }
 
-    void Running()
+    void GetMovementInput()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        movement = new Vector2(horizontal, vertical).normalized;
+    }
+
+    void HandleRunning()
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
             Speed = RunningSpeed;
-            activeSpeed = Speed;
-            playerMover();
         }
         else
         {
             Speed = WalkingSpeed;
+        }
+        
+        // Only update activeSpeed if the player isnt dashing
+        if (dashCounter <= 0)
+        {
             activeSpeed = Speed;
-            playerMover();
         }
     }
-    void playerMover()
+    
+    void HandleDashing()
     {
-
-        float vertical = Input.GetAxisRaw("Vertical");
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        movement = new Vector2(horizontal, vertical).normalized;
-        rb.linearVelocity = new Vector2(movement.x * activeSpeed, movement.y * activeSpeed) * Time.deltaTime;
-    }
-    void Dash()
-    {
-        
-        
-        if (dashCoolCounter <= 0 && dashCounter <= 0)
+        // Starts dash
+        if (Input.GetKeyDown(KeyCode.Space) && dashCoolCounter <= 0 && dashCounter <= 0)
+        {
+            // Only dash if there's movement input (prevents dashing in place)
+            if (movement.magnitude > 0.1f)
             {
                 dashCounter = dashLength;
                 activeSpeed = dashSpeed;
-                
             }
+        }
         
+        // Updates dash timer
         if (dashCounter > 0)
         {
             dashCounter -= Time.deltaTime;
@@ -83,12 +90,17 @@ public class PlayermOVEMENT : MonoBehaviour
                 dashCoolCounter = dashCooldown;
             }
         }
+        
+        // Updates dash cooldown
         if (dashCoolCounter > 0)
         {
             dashCoolCounter -= Time.deltaTime;
         }
     }
-
-
     
+    void MovePlayer()
+    {
+        // Applies movement
+        rb.linearVelocity = movement * activeSpeed * Time.deltaTime;
+    }
 }
