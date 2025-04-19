20651a6ef3114f5060c4d51dff403c13ac48578e
germ1nat <deniz_kaplan_arjen@hotmail.com>
2025-04-19 19:42:52 +0300

    yarrak

--- Assets/Scripts/Takedown.cs
+++ Assets/Scripts/Takedown.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Takedown : MonoBehaviour
 {
     public GameObject tDownButton;
+    public Rigidbody2D rbPlayer;
+    public Rigidbody2D rbEnemy;
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -11,7 +13,8 @@ public class Takedown : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E))
             {
                 tDownButton.SetActive(false);
-
+                rbEnemy.linearVelocity = new Vector2(0, 0);
+                rbPlayer.linearVelocity = new Vector2(0, 0);
 
             }
         }
