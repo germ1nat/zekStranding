d8d36d8bf7df0ce6b42d63b55f9e822d7f999500
germ1nat <deniz_kaplan_arjen@hotmail.com>
2025-04-19 08:57:16 +0300

    bunchofshit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,18 @@ public class PlayerMovement : MonoBehaviour
     public float RunningSpeed = 1125;
     private float Speed;
 
+    public bool isRunning = false;
+    public bool isDashing = false;
+    public float stamina = 100f;
+    public float recoveryRate = 20f;
+    public float recoverStamina;
+    public float onDashStaminaLost = 2f;
+    public float onRunStaminaLost = 0.8f;
+
+
     private float activeSpeed;
     public float dashSpeed = 2000; // Default value for dash speed
-
     public float dashLength = 0.5f, dashCooldown = 1f;
-
     private float dashCounter;
     private float dashCoolCounter;
 
@@ -24,14 +31,19 @@ public class PlayerMovement : MonoBehaviour
     {
         Speed = WalkingSpeed; // Initializessssss ssssssssspeed
         activeSpeed = Speed;
+        recoverStamina = recoveryRate;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
         // Gets movement input
         GetMovementInput();
-        
+
+        StaminaRecovery();
+        StaminaLoss();
+
         // Handles running / walking
         HandleRunning();
         
@@ -40,6 +52,7 @@ public class PlayerMovement : MonoBehaviour
         
         // Applies movement
         MovePlayer();
+
     }
 
     void GetMovementInput()
@@ -51,12 +64,16 @@ public class PlayerMovement : MonoBehaviour
 
     void HandleRunning()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        
+        
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > onRunStaminaLost)
         {
+            isRunning = true;
             Speed = RunningSpeed;
         }
         else
         {
+            isRunning = false;
             Speed = WalkingSpeed;
         }
         
@@ -70,22 +87,29 @@ public class PlayerMovement : MonoBehaviour
     void HandleDashing()
     {
         // Starts dash
-        if (Input.GetKeyDown(KeyCode.Space) && dashCoolCounter <= 0 && dashCounter <= 0)
+        if (Input.GetKeyDown(KeyCode.Space) && dashCoolCounter <= 0 && dashCounter <= 0 && stamina > onDashStaminaLost)
         {
+
             // Only dash if there's movement input (prevents dashing in place)
-            if (movement.magnitude > 0.1f)
-            {
-                dashCounter = dashLength;
-                activeSpeed = dashSpeed;
-            }
+            
+            
+                if (movement.magnitude > 0.1f)
+                {
+                    isDashing = true;
+                    dashCounter = dashLength;
+                    activeSpeed = dashSpeed;
+                }
+            
         }
         
         // Updates dash timer
         if (dashCounter > 0)
         {
+            
             dashCounter -= Time.deltaTime;
             if (dashCounter <= 0)
             {
+                isDashing = false;
                 activeSpeed = Speed;
                 dashCoolCounter = dashCooldown;
             }
@@ -103,4 +127,35 @@ public class PlayerMovement : MonoBehaviour
         // Applies movement
         rb.linearVelocity = movement * activeSpeed * Time.deltaTime;
     }
+    void StaminaLoss()
+    {
+        if (isDashing)
+        {
+            stamina -= onDashStaminaLost;
+            //recoverStamina = 0;
+
+        }
+
+        if (isRunning)
+        {
+            stamina -= onRunStaminaLost;
+            recoverStamina = 0;
+
+        }
+        stamina = Mathf.Clamp(stamina, 0, 100);
+        print(stamina);
+    }
+    void StaminaRecovery()
+    {
+
+        if (!isRunning && !isDashing && !Input.GetKey(KeyCode.LeftShift))
+        {
+            recoverStamina = recoveryRate;
+            stamina += recoveryRate * Time.deltaTime;
+        }
+    }
+
+
+    
+    
 }
